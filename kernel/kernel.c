ff06902dd7a6d64e20516619727bc338e1a0efa5
DonSanoj <sanojaminda@gmail.com>
2026-03-15 21:24:19 +0530

    Refactor kernel_main to utilize draw_ui for UI rendering

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,16 +1,8 @@
+extern void draw_ui();
+
 void kernel_main() {
+    draw_ui();
 
-    char *video = (char*) 0xb8000;
-
-    video[0] = 'A';
-    video[1] = 0x07;
-
-    video[2] = 'O';
-    video[3] = 0x07;
-
-    video[4] = 'S';
-    video[5] = 0x07;
-
-    while (1);
-    
+    while (1) {
+    }
 }
